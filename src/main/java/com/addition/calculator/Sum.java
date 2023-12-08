8950e4804ad1fd78cc3568b8a6eb749cd5f95aa5
Sandeep Kumar <sandeepnkumar21@gmail.com>
2023-12-09 00:40:29 +0530

    this is the stash demo

--- src/main/java/com/addition/calculator/Sum.java
+++ src/main/java/com/addition/calculator/Sum.java
@@ -12,10 +12,64 @@ public class Sum {
 		
 		return c;
 	}
+	
+	public int sub1(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+	public int sub2(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+	public int sub3(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+	public int sub4(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+	public int sub5(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
 
 	
+<<<<<<< Updated upstream
 	// Important
 	
+=======
+	public int div1(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+	public int div2(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+	public int div3(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+	public int div4(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+	public int div5(int a, int b) {
+		int c= a-b;
+		System.out.println("Substraction Service: of a and b" + c);
+		return c;
+	}
+>>>>>>> Stashed changes
 	
 	
 	public int Division(int a , int b)
